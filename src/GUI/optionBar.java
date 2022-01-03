20d7a0cd94eff6d44c0fe9b4e3bdbe72709e35f2
daniieelgs <daniieelgs@gmail.com>
2022-01-03 14:27:36 +0100

    Nuevas opciones de menu. 'Nuevo Juego' y 'Pausar'

--- src/GUI/optionBar.java
+++ src/GUI/optionBar.java
@@ -1,16 +1,28 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.KeyStroke;
 
 public class optionBar extends JPanel{
 	
@@ -18,7 +30,8 @@ public class optionBar extends JPanel{
 	private JRadioButton beginner, medium, expert;
 	private ButtonGroup levels;
 	private panel content;
-	private JFrame frame;
+	private static JFrame frame;
+	private static counter count;
 	
 	public optionBar(panel content, JFrame frame) {
 				
@@ -26,11 +39,19 @@ public class optionBar extends JPanel{
 		
 		this.content=content;
 		
-		this.frame=frame;
+		count=content.getCount();
+		
+		optionBar.frame=frame;
 		
 		JMenuBar bar=new JMenuBar();
 		
 		JMenu game=new JMenu("Juego");
+
+		JMenuItem new_game=new JMenuItem("Nuevo Juego");
+		JMenuItem stop=new JMenuItem("Pausar");
+				
+		new_game.setAccelerator(KeyStroke.getKeyStroke("F2"));
+		stop.setAccelerator(KeyStroke.getKeyStroke("P"));
 		
 		levels=new ButtonGroup();
 		
@@ -48,13 +69,29 @@ public class optionBar extends JPanel{
 		medium.addActionListener(event);
 		expert.addActionListener(event);
 		
+		game.add(new_game);
+		game.add(stop);
+		
+		game.add(new JSeparator());
+		
 		game.add(beginner);
 		game.add(medium);
 		game.add(expert);
-		
+				
 		bar.add(game);
 		
 		add(bar, BorderLayout.CENTER);
+				
+		stop.addActionListener(new stopAction());
+		
+		new_game.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				count.reset();
+				
+			}
+		});
 		
 	}
 	
@@ -78,6 +115,90 @@ public class optionBar extends JPanel{
 				frame.setBounds(width/2-(width/4*2)/2, 0, width/4*2, (height/2)*2);
 			}
 			
+		}
+		
+	}
+	
+	private static class stopAction implements ActionListener{
+		
+		private static boolean stop;
+		private JDialog stopDialog;
+		
+		public void actionPerformed(ActionEvent e) {
+			
+			if(count.isCounting() || stop) {
+				
+				stop=!stop;
+				
+				if(stop) {
+					
+					count.stopTimer();
+					
+					stopFrame();
+					
+				}else {
+					
+					stopDialog.dispose();
+					count.startTimer();
+					
+				}
+				
+			}
+			
+		}
+		
+		void stopFrame() {
+			
+			stopDialog=new JDialog();
+			
+			ActionListener event=this;
+			
+			stopDialog.add(new JPanel(new BorderLayout()) {
+	
+				private static final long serialVersionUID = 1L;
+
+				{
+					setBackground(Color.black);
+					
+					setBorder(BorderFactory.createLineBorder(Color.WHITE, 5));
+					
+					JLabel l=new JLabel("Pausa", JLabel.CENTER);
+					l.setForeground(Color.WHITE);
+					l.setFont(new Font("Verdana", Font.BOLD, 25));
+					l.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+					
+					add(l, BorderLayout.NORTH);
+					
+					JPanel p=new JPanel();
+					
+					p.setOpaque(false);
+					
+					JButton play=new JButton("Reanudar");
+					
+					play.setFocusPainted(false);
+					play.setBorderPainted(false); 
+					play.setBackground(Color.WHITE);
+					play.setCursor(new Cursor(Cursor.HAND_CURSOR));
+					play.addActionListener(event);
+					
+					p.add(play);
+					
+					add(p, BorderLayout.CENTER);
+					
+					
+				}
+				
+			});
+			
+			Point p=frame.getLocation();
+			Dimension d=frame.getSize();
+			
+			stopDialog.setBounds(p.x+40, p.y+40, d.width-80, d.height-80);
+			
+			stopDialog.setUndecorated(true);
+			
+			stopDialog.setModal(true);
+			stopDialog.setVisible(true);
 		}
 		
 	}
